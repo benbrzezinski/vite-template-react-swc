f1980479c2d859fe5f117b7dc428f417c40a4d90
Beniamin Brzeziński <benek9311@wp.pl>
2023-06-21 13:45:05 +0200

    Changed deploy from gh-pages to netlify and added react-router-dom

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,28 @@
+import { Routes, Route, NavLink } from "react-router-dom";
+
 const App = () => (
-  <div style={{ fontSize: 50, textAlign: "center" }}>Vite + React</div>
+  <>
+    <header>
+      <nav>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/page2" style={{ marginLeft: 10 }}>
+          Page 2
+        </NavLink>
+      </nav>
+    </header>
+    <main>
+      <Routes>
+        <Route
+          path="/"
+          element={<div style={{ marginTop: 10, marginLeft: 10 }}>Home</div>}
+        />
+        <Route
+          path="/page2"
+          element={<div style={{ marginTop: 10, marginLeft: 10 }}>Page 2</div>}
+        />
+      </Routes>
+    </main>
+  </>
 );
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import React from "react";
+import { StrictMode } from "react";
+import { BrowserRouter } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import App from "./components/App";
 import "./main.scss";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
 );
